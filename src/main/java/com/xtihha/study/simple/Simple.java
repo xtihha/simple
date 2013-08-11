7eae450e4a11a872b7ba69ed378e83e7747c9eac
zhangxiaohu <zhangxiaohu@meituan.com>
2013-08-11 15:03:57 +0800

    project archetype

---
+++ src/main/java/com/xtihha/study/simple/Simple.java
@@ -0,0 +1,17 @@
+package com.xtihha.study.simple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: zhangxiaohu
+ * Date: 13-8-11
+ * Time: 上午11:12
+ */
+public class Simple {
+    private static final Logger logger = LoggerFactory.getLogger(Simple.class);
+
+    public static void main(String []args){
+        logger.info("Simple main class test");
+    }
+}
